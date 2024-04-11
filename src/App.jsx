6fbfbe2c13931c5e178c38bd4ad16d3632373fbf
Leonid Almanza <LeonidAlmanza@gmail.com>
2024-04-10 20:40:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Tarjeta from './components/tarjeta.jsx';
+
+
+function App() {
+  const datosUsuario = [
+    {
+     nombre:'Leonid Almanza', 
+     empresa:'Idependiente',
+     informacion:'Leonid Almanza ipsum dolor sit amet consectetur adipisicing elit. Quidem aperiam sunt inventore aliquid velit eveniet dolores minima sapiente quibusdam! Corrupti, incidunt distinctio placeat eius iure mollitia tenetur deleniti rem molestiae!',
+     foto:''
+    },
+    {
+      nombre:'Sarah China en Nigeria', 
+      empresa:'Ingeniera de Software ChatDesk',
+      informacion:'Sarah China ipsum dolor sit amet consectetur adipisicing elit. Quidem aperiam sunt inventore aliquid velit eveniet dolores minima sapiente quibusdam! Corrupti, incidunt distinctio placeat eius iure mollitia tenetur deleniti rem molestiae!',
+      foto:''
+     },
+     {
+      nombre:'Emma Bostian en Suecia', 
+      empresa:'Ingeniera de Software Spotify',
+      informacion:'Emma Bostian ipsum dolor sit amet consectetur adipisicing elit. Quidem aperiam sunt inventore aliquid velit eveniet dolores minima sapiente quibusdam! Corrupti, incidunt distinctio placeat eius iure mollitia tenetur deleniti rem molestiae!',
+      foto:''
+     }
+  ]; 
+
+  /* Recorrer el Vector para ir llamandolo y motrando la ifnormación en la tarjeta*/
+  return (
+    <div>
+      {datosUsuario.map((data, index) => (
+        <Tarjeta nombre={data.nombre} empresa={data.empresa} informacion={data.informacion}/>
+        ))}   
+     
+  </div>
+  );
+}
+
+export default App
---
+++ src/components/tarjeta.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../stylesheets/tarjeta.css';
+
+function Tarjeta(props){
+
+    return(
+        <div className="tarjeta">
+            <img src="" alt="Foto de la persona" className="foto-tarjeta"/>
+            <div className="contenido-tarjeta">
+                <h2 className="nombre">{props.nombre}</h2>
+                <p className="profesion">{props.empresa}</p>
+                <p className="descripcion">{props.informacion}</p>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Tarjeta;
+
+/*
+ <Tarjeta nombre='Leonid Almanza' empresa='Independiente' informacion='Soy ingeniero de sistemas independiente, por lo cual no estoy en ninguna empresa'/>
+*/
